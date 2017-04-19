8c1897501fd0a124f0963a45a74f64938a83ff6e
David Harper <adh@sanger.ac.uk>
2017-04-19 13:49:06 +0100

    Add utility scripts.

---
+++ mailtool/scripts/listfolders.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+BASE_DIR="${SCRIPT_DIR}/.."
+
+CLASSES_DIR=${BASE_DIR}/classes
+LIB_DIR=${BASE_DIR}/lib
+
+CLASSPATH="${CLASSES_DIR}:${LIB_DIR}/*"
+
+APP_CLASS="com.obliquity.mailtool.ListFolders"
+
+java ${JAVA_OPTS} -Djava.awt.headless=true -D"mail.imap.ssl.trust=*" \
+    -classpath "${CLASSPATH}" ${APP_CLASS} \
+    "$@"
